9b9b40c8fce73aabd6c96dcee08baf2df31cb42f
dreadcrumb <leander.suda@aon.at>
2016-01-07 18:36:08 +0100

    minor changes to main

--- src/AdventureTest.java
+++ src/AdventureTest.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
+import encounters.*;
 
 public class AdventureTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		standardEncounter[] encounterArray = { new e1_start(),
+				new e2_friends(), new e3_assault() };
+
+		System.out.println("Choose your race, sucker");
+
+		Scanner scanner = new Scanner(System.in);
+		String race, name;
+		do {
+			System.out.println("Enter your race: [Dwarf, Elf, Orc, Troll]");
+			race = scanner.nextLine();
+		} while (race == null || race == "");
+		do {
+			System.out.println("Enter your name, " + race);
+			name = scanner.nextLine();
+		} while (name == null || name == "");
+
+		Player player = new Player(race, name);
 
 	}
 
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,18 @@
 
 public class Player {
 
+	String race, name;
+	
+	public Player(String _race, String _name) {
+		race = _race;
+		_name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getRace() {
+		return race;
+	}
 }
